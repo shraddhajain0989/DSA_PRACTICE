79cacbb069a6468098ee5aaeffca514362d6e949
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-15 20:53:05 +0530

    reverse the number by another

---
+++ day-5/sorting/reverse_bysolution.java
@@ -0,0 +1,12 @@
+public class solution {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+              
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
---
+++ day-5/sorting/selectionsort.java
@@ -0,0 +1,27 @@
+public class selectionsort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        selectionSort(arr);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+// Selection Sort in Java
