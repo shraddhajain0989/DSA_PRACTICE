2a00a47df36e844dd12e6c7e7b0f640b21ec0373
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-18 08:34:32 +0530

    all codes are of 2d

---
+++ day-6/max_2d.java
@@ -0,0 +1,16 @@
+public class max_2d {
+    public static void main(String[]args){
+        int arr[][]={{1,2,3},
+                    {4,5,6}
+        };
+        int max = arr[0][0];
+        for (int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.print(" max = " + max);
+    }
+}
---
+++ day-6/sum_arr.java
@@ -0,0 +1,16 @@
+public class sum_arr {
+    public static void main(String[]args){
+        int [] [] arr ={
+            {1,2},
+            {3,4}};
+    int sum =0;
+    for (int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            sum += arr[i][j];
+            
+        }
+        
+    }
+    System.out.print(" sum = " + sum);
+}
+}
