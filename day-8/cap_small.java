deb69a2a9a215aee56bbc62e9e20b1f7136a34f6
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-22 11:49:13 +0530

    conversion to lowr and upper

---
+++ day-8/cap_small.java
@@ -0,0 +1,8 @@
+public class cap_small {
+    public static void main(String[] args) {
+        String s = "hello world";
+        System.out.println("Original: '" +s +"'");
+        System.out.println("toUpperCase: " +s.toUpperCase());
+        System.out.println("toLowerCase: " +s.toLowerCase());
+    }
+}
