def6a9fa8df051dcaf7e28dde082e76af92d989a
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-22 12:15:43 +0530

    by using loop

---
+++ day-8/reverse_easy.java
@@ -0,0 +1,9 @@
+public class reverse_easy {
+    public static void main(String[] args) {
+        String s = "Java ";
+       for(int i=s.length()-1;i>=0;i--){
+        System.out.print(s.charAt(i));
+       }
+    }
+}
+
