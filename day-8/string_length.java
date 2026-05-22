64cc8ac940028d5e6ac60ea3674d3cd8c29fd30f
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-22 10:28:07 +0530

    how many index with value print

---
+++ day-8/string_length.java
@@ -0,0 +1,9 @@
+public class string_length {
+    public static void main(String[] args) {
+        String s1 = "Hello, World!";
+        String s2 = "Java Programming";
+        
+        System.out.println("The length of the first string is: " + s1.length());
+        System.out.println("The length of the second string is: " + s2.length() );
+    }
+}
