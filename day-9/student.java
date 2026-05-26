3f6eaf0edde04e695449c2b42cd5c69b152fc306
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-26 19:00:06 +0530

    encapsulation method example

---
+++ day-9/student.java
@@ -0,0 +1,35 @@
+public class student {
+    private String name;
+    private int rollno;
+    public void setName(String name) {
+        if(name !=null && !name.trim().isEmpty()) {
+            this.name = name;
+        } else {
+            System.out.println("Invalid name");
+        }
+    }
+    public  void setRollno(int rollno) {
+        if(rollno > 0) {
+            this.rollno = rollno;
+        } else {
+            System.out.println("Invalid roll number");
+        }
+    }
+        public String getName() {
+            return name;
+        }
+        public int getRollno() {
+            return rollno;
+        }
+}
+    class Main1 {
+        public static void main(String[] args) {
+            student s = new student();
+            s.setName("John Doe");
+            s.setRollno(12345);
+            System.out.println("Name: " + s.getName());
+            System.out.println("Roll No: " + s.getRollno());
+        }
+    }
+
+
