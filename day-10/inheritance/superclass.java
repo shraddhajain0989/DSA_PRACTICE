18e4d965c2e65fd3ad54273b696e6100aeda3fe4
Shraddha Jain <shraddhajain0989@gmail.com>
2026-05-29 12:26:19 +0530

    notes

--- day-10/inheritance/superclass.java
+++ day-10/inheritance/superclass.java
@@ -1,6 +1,21 @@
+import.java.util.Scanner;
 class person_constructor{
     void emp_constructor(){
-        PR
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+class employee_constructor extends person_constructor{
+    void emp_constructor(){
+        super.emp_constructor();
+        Scanner sc = new Scanner(System.in);
+        String company = sc.nextLine();
+        int salary = sc.nextInt();
+        System.out.println("Company: " + company);
+        System.out.println("Salary: " + salary);
     }
 }
 
